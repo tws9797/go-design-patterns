70d4d5d20c8dfd1436b15b63336df2a5baf61466
Tee Wen Seng <wensengtee@gmail.com>
2022-10-18 11:40:10 +0800

    factory-method

--- factory-method/iGun.go
+++ factory-method/iGun.go
@@ -1,5 +1,6 @@
 package factory_method
 
+// https://refactoring.guru/design-patterns/factory-method
 // https://golangbyexample.com/golang-factory-design-pattern/
 // Provides an interface for creating an objects in a superclass
 // but allows subclasses to alter the type of objects that will be created
