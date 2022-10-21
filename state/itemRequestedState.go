80c5ad7063a5b435f278a3b917d6748f4c1efb34
Wen Seng Tee <wensengtee@gmail.com>
2022-10-21 21:11:01 +0800

    state

---
+++ state/itemRequestedState.go
@@ -0,0 +1,28 @@
+package state
+
+import "fmt"
+
+type ItemRequestedState struct {
+	VendingMachine *VendingMachine
+}
+
+func (i *ItemRequestedState) RequestItem() error {
+	return fmt.Errorf("item already requested")
+}
+
+func (i *ItemRequestedState) AddItem(count int) error {
+	return fmt.Errorf("item Dispense in progress")
+}
+
+func (i *ItemRequestedState) InsertMoney(money int) error {
+	if money < i.VendingMachine.ItemPrice {
+		fmt.Errorf("inserted money is less. Please insert %d", i.VendingMachine.ItemPrice)
+	}
+	fmt.Println("Money entered is ok")
+	i.VendingMachine.SetState(i.VendingMachine.HasMoney)
+	return nil
+}
+
+func (i *ItemRequestedState) DispenseItem() error {
+	return fmt.Errorf("please insert money first")
+}
