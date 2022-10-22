76561cbfbff5e159183b640de81a4726c075b831
Wen Seng Tee <wensengtee@gmail.com>
2022-10-22 11:37:04 +0800

    strategy

--- adapter/windowsAdapter.go
+++ adapter/windowsAdapter.go
@@ -2,6 +2,10 @@ package adapter
 
 import "fmt"
 
+// https://refactoring.guru/design-patterns/adapter
+// https://golangbyexample.com/adapter-design-pattern-go/
+// Allows objects with incompatible interfaces to collaborate
+
 type WindowsAdapter struct {
 	windowMachine *Windows
 }
---
+++ strategy/fifo.go
@@ -0,0 +1,10 @@
+package strategy
+
+import "fmt"
+
+type Fifo struct {
+}
+
+func (l *Fifo) Evict(c *Cache) {
+	fmt.Println("Evicting by fifo strategy")
+}
